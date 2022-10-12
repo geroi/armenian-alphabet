cd0544ccaf2e5a12960cf28028eaa5757eee2317
Alex Kashmatov <akashmatov@gmail.com>
2022-10-13 01:41:16 +0400

    improvs

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,8 +1,9 @@
 window.addEventListener("load", ()=>{
     document.querySelectorAll('.hint-button').forEach((element)=>{
         element.addEventListener("click", ()=>{
-            let target = document.querySelector(".hint-text[data-id='"+ element.dataset.id + "']")
-            target.classList.remove('d-none')
+            let target = document.querySelector("input[data-id='"+ element.dataset.id + "']")
+            target.value = target.pattern;
+            target.dispatchEvent(new Event('input'));
         })
     })
 
